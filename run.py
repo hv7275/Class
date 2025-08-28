5ebf77a9f116d82c0e891bd3db18a859c9d315c8
hv7275 <hv7275384@gmail.com>
2025-08-29 00:36:07 +0530

    Rendering Template

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return Response('Hi, Khushi')
-    
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    context = [
+        {
+            'title': 'About Us',
+            'content': 'This is the about page.'
+        },
+        {
+            'title':'Harsh',
+            'content':"Harsh Khushi"
+        }
+    ]
+    return render_template('about.html', rams=context)
+
 app.run(debug=True)
 
